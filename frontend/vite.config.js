8e78776a4d3f5b4c16892f041d99ceec25250082
Nikhil Shinde <160251430+nikkkhilllit@users.noreply.github.com>
2025-08-15 20:03:07 +0530

    vite.config.js update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,21 @@
 import path from "path"
+import { fileURLToPath } from "url"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+// ESM-safe __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
+  base: "/", // important for deployment
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // render static site expects dist
   },
 })
