95771b8a53b80f66465a01543fafdf75ea95afc0
Nikhil Shinde <160251430+nikkkhilllit@users.noreply.github.com>
2025-09-28 15:00:55 +0530

    Update Browse.jsx

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -5,32 +5,30 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setSearchedQuery } from '@/redux/jobSlice';
 import useGetAllJobs from '@/hooks/useGetAllJobs';
 
-// const randomJobs = [1, 2,45];
-
 const Browse = () => {
     useGetAllJobs();
-    const {allJobs} = useSelector(store=>store.job);
+    const { allJobs } = useSelector(store => store.job);
     const dispatch = useDispatch();
-    useEffect(()=>{
-        return ()=>{
+
+    useEffect(() => {
+        return () => {
             dispatch(setSearchedQuery(""));
         }
-    },[])
+    }, [dispatch]);
+
     return (
         <div>
             <Navbar />
-            <div className='max-w-7xl mx-auto my-10'>
-                <h1 className='font-bold text-xl my-10'>Search Results ({allJobs.length})</h1>
-                <div className='grid grid-cols-3 gap-4'>
-                    {
-                        allJobs.map((job) => {
-                            return (
-                                <Job key={job._id} job={job}/>
-                            )
-                        })
-                    }
+            <div className="max-w-7xl mx-auto my-8 px-4 sm:px-6 lg:px-8">
+                <h1 className="font-bold text-lg sm:text-xl mb-6">
+                    Search Results ({allJobs.length})
+                </h1>
+                
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {allJobs.map((job) => (
+                        <Job key={job._id} job={job} />
+                    ))}
                 </div>
-
             </div>
         </div>
     )
